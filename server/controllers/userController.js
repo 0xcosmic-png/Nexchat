0d85547270e629e024dafc514a3d56a48f7b25f8
0xCosmic <kahanpatel0099@gmail.com>
2026-08-20 23:47:22 +0530

    Build backend with dependencies and controllers

---
+++ server/controllers/userController.js
@@ -0,0 +1,95 @@
+const User = require("../models/userModel");
+//used for encryption of the password 
+const bcrypt = require("bcrypt");
+
+module.exports.login = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+    //check for user if he'she is excisting or not
+
+    if (!user)
+      return res.json({ msg: "Incorrect Username or Password", status: false });
+
+    //checking pass is same we get as both backend and frontend
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid)
+      return res.json({ msg: "Incorrect Username or Password", status: false });
+
+    delete user.password;
+    return res.json({ status: true, user });
+  } catch (ex) {
+    next(ex);
+  }
+};
+
+module.exports.register = async (req, res, next) => {
+    try {
+      const { username, email, password } = req.body;
+      const usernameCheck = await User.findOne({ username });
+      if (usernameCheck)
+        return res.json({ msg: "Username already used", status: false });
+      const emailCheck = await User.findOne({ email });
+      if (emailCheck)
+        return res.json({ msg: "Email already used", status: false });
+  
+      //hashing the password we will pass the password and salt value which is 10 type of encryption
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = await User.create({
+        email,
+        username,
+        password: hashedPassword,
+      });
+      delete user.password;
+      return res.json({ status: true, user });
+    } catch (ex) {
+      next(ex);
+    }
+  };
+
+
+  module.exports.setAvatar = async (req, res, next) => {
+    try {
+      const userId = req.params.id;
+      const avatarImage = req.body.image;
+      const userData = await User.findByIdAndUpdate(
+        userId,
+        {
+          isAvatarImageSet: true,
+          avatarImage,
+        },
+        { new: true }
+      );
+      return res.json({
+        isSet: userData.isAvatarImageSet,
+        image: userData.avatarImage,
+      });
+    } catch (ex) {
+      next(ex);
+    }
+  };
+
+  module.exports.getAllUsers = async (req, res, next) => {
+    try {
+      const users = await User.find({ _id: { $ne: req.params.id } }).select([
+        "email",
+        "username",
+        "avatarImage",
+        "_id",
+      ]);
+      return res.json(users);
+    } catch (ex) {
+      next(ex);
+    }
+  };
+
+  module.exports.logOut = (req, res, next) => {
+    try {
+      if (!req.params.id) return res.json({ msg: "User id is required " });
+      onlineUsers.delete(req.params.id);
+      return res.status(200).send();
+    } catch (ex) {
+      next(ex);
+    }
+  };
